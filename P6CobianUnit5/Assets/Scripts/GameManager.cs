8ffbc3380e33326514b897f84b3a5651215d0900
bestatclash <743376@student.dusd.net>
2026-05-06 13:47:17 -0700

    did 5.1
    started 5.2

--- P6CobianUnit5/Assets/Scripts/GameManager.cs
+++ P6CobianUnit5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
-
+using TMpro;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
     }
 
     // Update is called once per frame
