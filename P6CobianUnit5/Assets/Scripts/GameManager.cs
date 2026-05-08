04597ddb1744324d1a63a6b4ad9ce48b2c57e1b8
bestatclash <743376@student.dusd.net>
2026-05-08 13:48:09 -0700

    did some work
    did work

--- P6CobianUnit5/Assets/Scripts/GameManager.cs
+++ P6CobianUnit5/Assets/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
-using TMpro;
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
+        UpdateScore(0);
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
@@ -28,6 +30,15 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
+
+            UpdateScore(5);
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
